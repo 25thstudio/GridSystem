af818b370fa25b28be4709e60067ed48f63a3f14
Paulo Sigrist <paulo.sigrist@gmail.com>
2021-06-04 16:01:55 -0300

    POW-34
    GetGroupedComponents

--- Runtime/Scripts/UI/GridColorMap.cs
+++ Runtime/Scripts/UI/GridColorMap.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace The25thStudio.GridSystem.UI
@@ -31,5 +32,57 @@ namespace The25thStudio.GridSystem.UI
         public IEnumerable<Color32> Colors => _dict.Keys;
 
         public List<GameObject> this[Color32 color] => _dict.ContainsKey(color) ? _dict[color] : default;
+
+        public Dictionary<TKey, List<TType>> GetGroupedComponents<TKey, TType>(Func<Color32, GameObject, TKey> funcGroup)
+        {
+            var dict = new Dictionary<TKey, List<TType>>();
+
+            foreach (var color in _dict.Keys)
+            {
+                foreach (var item in _dict[color])
+                {
+                    var type = item.GetComponent<TType>();
+                    if (type is null) continue;
+
+                    List<TType> list;
+                    var group = funcGroup(color, item);
+                    
+                    if (dict.ContainsKey(group))
+                    {
+                        list = dict[group];
+                    }
+                    else
+                    {
+                        list = new List<TType>();
+                        dict[group] = list;
+                    }
+
+                    list.Add(type);
+                }
+            }
+
+            return dict;
+        }
+
+
+        public Dictionary<TKey, TType> GetGroupedComponent<TKey, TType>(Func<Color32, GameObject, TKey> funcGroup)
+        {
+            var dict = new Dictionary<TKey, TType>();
+
+            foreach (var color in _dict.Keys)
+            {
+                foreach (var item in _dict[color])
+                {
+                    var type = item.GetComponent<TType>();
+                    if (type is null) continue;
+                    
+                    var group = funcGroup(color, item);
+                    dict[group] = type;
+                }
+            }
+
+            return dict;
+        }
+
     }
 }
